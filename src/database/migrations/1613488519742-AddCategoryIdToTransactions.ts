2559992be7abda5c7469090bb14af2a29c15ed7e
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-02-16 12:16:58 -0300

    Starting the challenge

---
+++ src/database/migrations/1613488519742-AddCategoryIdToTransactions.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class AddCategoryIdToTransactions1613488519742
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
